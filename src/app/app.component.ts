59e5a951b335b0f462736acbe7e12a9107ee9121
Marius Mircea <mariusmircea@outlook.com>
2019-06-10 11:48:28 +0300

    Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,72 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BusyMachinesInterview';
+  selectedFunction = 0;
+  a = 0;
+  b = 0;
+  c = 0;
+  stringToReverse = '';
+  res = '';
+
+  onSubmit() {
+    switch (Number(this.selectedFunction)) {
+      case 0:
+        this.res = this.sum(this.a, this.b) + '';
+        break;
+      case 1:
+        this.res = this.max(this.a, this.b, this.c) + '';
+        break;
+      case 2:
+        this.res = this.isPrime(this.a) === true ? `${this.a} is a Prime Number` : `${this.a} is NOT a Prime Number`;
+        break;
+      case 3:
+        this.res = this.reversed(this.stringToReverse);
+        break;
+    }
+  }
+
+  private isPrime(a: number) {
+    let i;
+    let dividedTimes = 0;
+
+    if (a <= 1) { return false; }
+    if (a <= 3 ) { return true; }
+
+    for (i = 2; i <= a / 2; i++) {
+      if (a % i === 0) {
+        dividedTimes += 1;
+      }
+    }
+    if (dividedTimes >= 1) {
+      return false;
+    }
+    return true;
+  }
+
+  private sum(a, b) {
+    return Number(a) + Number(b);
+  }
+
+  private max(a: number, b: number, c: number) {
+    let maxNr: number = a;
+    if (maxNr < b) {
+      maxNr = b;
+    }
+    if (maxNr < c) {
+      maxNr = c;
+    }
+    return maxNr;
+  }
+
+  private reversed(stringToReverse: string) {
+    // return stringToReverse
+    //   .split('')
+    //   .reverse()
+    //   .join('');
+    let reversedString = '';
+    for (let i = stringToReverse.length - 1; i >= 0 ; --i) {
+      reversedString += stringToReverse.charAt(i);
+    }
+    return reversedString;
+  }
 }
